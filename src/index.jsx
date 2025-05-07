80ea28bcc3eaf8d7828f4364177d94e0686d7e08
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2025-05-07 11:25:02 +0530

    updating to HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 import App from "./App.jsx";
 import "./index.css";
 
+const basename =
+  process.env.NODE_ENV === "production" ? "/small-react-projects" : "";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/small-react-projects">
+  <HashRouter basename={basename}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
--- src/pages/tenzies/Die.jsx
+++ src/pages/tenzies/Die.jsx
@@ -2,6 +2,22 @@ import React from "react";
 
 import TenziesCss from "./tenzies.module.css";
 
+import die1 from "./assets/die-1.svg";
+import die2 from "./assets/die-2.svg";
+import die3 from "./assets/die-3.svg";
+import die4 from "./assets/die-4.svg";
+import die5 from "./assets/die-5.svg";
+import die6 from "./assets/die-6.svg";
+
+const dieImages = {
+  1: die1,
+  2: die2,
+  3: die3,
+  4: die4,
+  5: die5,
+  6: die6,
+};
+
 export default function Die(props) {
   const styles = {
     filter: `brightness(${props.isHeld ? 0.75 : 1})`,
@@ -21,10 +37,7 @@ export default function Die(props) {
       } `}
     >
       {/* {props.value} */}
-      <img
-        src={`src/assets/tenzies/die-${props.value}.svg`}
-        alt={props.value}
-      />
+      <img src={dieImages[props.value]} alt={props.value} />
     </button>
   );
 }
