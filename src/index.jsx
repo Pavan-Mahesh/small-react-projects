0efb12aa98028329cb366c8cf0e80868ec7a902e
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2025-05-07 11:38:25 +0530

    changes specific to vite

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 const basename =
-  process.env.NODE_ENV === "production" ? "/small-react-projects" : "";
+  import.meta.env.MODE === "production" ? "/small-react-projects" : "";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter basename={basename}>
